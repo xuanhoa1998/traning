f3f9c30f6e7a2a2e0e8c32908dafdd5ebb3ea40c
dothinh <dinhthinh1712@gmail.com>
2023-02-21 20:55:37 +0700

    temp

--- apps/demologin/serializers.py
+++ apps/demologin/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model # If used custom user model
 
+from apps.demologin.models.user import User
+
 UserModel = get_user_model()
 
 
@@ -23,3 +25,29 @@ class UserSerializer(serializers.ModelSerializer):
         model = UserModel
         # Tuple of serialized model fields (see link [2])
         fields = ("id", "username", "password", "email")
+
+
+class ModelSerializer:
+    pass
+
+
+class UserPasswordSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            'password'
+        ]
+
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
